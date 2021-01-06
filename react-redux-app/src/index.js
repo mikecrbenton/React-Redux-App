c27079f5947ff9b8af9943c63f23af73452891e8
mikecrbenton <mikecrbenton@hotmail.com>
2021-01-05 18:45:42 -0600

    verified mvp finished

--- react-redux-app/src/index.js
+++ react-redux-app/src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import { dogReducer } from './reducers/dogReducer';
+
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from 'react-redux';
 import thunk from "redux-thunk";
